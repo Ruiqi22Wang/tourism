7d16e54fb487757b4789fc086aa7801e68344b98
chrimiway <1301235225@qq.com>
2018-11-04 15:04:20 -0500

    convert hotel address to city (test tomorrow (rate limit))

---
+++ convert_to_city.py
@@ -0,0 +1,24 @@
+from geopy.geocoders import Nominatim
+import pandas as pd
+
+hotel_addr = pd.read_csv("hotel_addr.csv")
+
+geolocator = Nominatim(timeout=200)
+
+#hotel_addr.dropna()
+
+with open("hotel_city.txt", "a") as city:
+    for index, addr in hotel_addr.dropna().iterrows():
+        # print (addr["lat"], addr["lng"])
+        location = geolocator.reverse("{0},{1}".format(addr["lat"], addr["lng"]))
+        if "city" in location.raw["address"]:
+            print(addr["Hotel_Address"]+ "@" + location.raw["address"]["city"] + "\n")
+            city.write(addr["Hotel_Address"]+ ", " + location.raw["address"]["city"] + "\n")
+        elif "county" in location.raw["address"]:
+            print(addr["Hotel_Address"]+ "@" + location.raw["address"]["county"] + "\n")
+            city.write(addr["Hotel_Address"]+ ", " + location.raw["address"]["county"] + "\n")
+        else:
+            print(addr["Hotel_Address"]+ "@" + location.raw["address"]["city_district"] + "\n")
+            city.write(addr["Hotel_Address"]+ ", " + location.raw["address"]["city_district"] + "\n")
+        pass
+    pass
