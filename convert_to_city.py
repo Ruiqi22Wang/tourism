b8c4b36e3cdfef9edccc5aa423ae61397709fdf6
chrimiway <1301235225@qq.com>
2018-11-04 20:23:38 -0500

    new

--- convert_to_city.py
+++ convert_to_city.py
@@ -22,3 +22,19 @@ with open("hotel_city.txt", "a") as city:
             city.write(addr["Hotel_Address"]+ ", " + location.raw["address"]["city_district"] + "\n")
         pass
     pass
+
+with open("hotel_city.txt", "a") as city:
+    for index, addr in hotel_addr.iloc[1303:].dropna().iterrows():
+        # print (addr["lat"], addr["lng"])
+        location = geolocator.reverse("{0},{1}".format(addr["lat"], addr["lng"]))
+        if "city" in location.raw["address"]:
+            print(addr["Hotel_Address"]+ "@" + location.raw["address"]["city"] + "\n")
+            city.write(addr["Hotel_Address"]+ ", " + location.raw["address"]["city"] + "\n")
+        elif "county" in location.raw["address"]:
+            print(addr["Hotel_Address"]+ "@" + location.raw["address"]["county"] + "\n")
+            city.write(addr["Hotel_Address"]+ ", " + location.raw["address"]["county"] + "\n")
+        else:
+            print(addr["Hotel_Address"]+ "@" + location.raw["address"]["city_district"] + "\n")
+            city.write(addr["Hotel_Address"]+ ", " + location.raw["address"]["city_district"] + "\n")
+        pass
+    pass
