b3c382d62f79a8acc3e6067e0d7ceca9e79e877e
chrimiway <1301235225@qq.com>
2018-11-06 21:19:03 -0500

    sentiment across time for diff city

--- hotal_analysis.R
+++ hotal_analysis.R
@@ -2,6 +2,7 @@ setwd("~/Desktop/Graduate/duke/MIDS/semester 1/text-scraping/project/tourism")
 library(tidyverse)
 library(tidytext)
 library(dplyr)
+library(lubridate)
 hotel = read_csv("Hotel_Reviews.csv")
 
 ## extract address and coordinates
@@ -39,7 +40,7 @@ for (i in 1:length(hotel$city)) {
   } else {
     print(paste(toString(i),"matched"))
   }
-} 
+}
 
 
 # delete positive reviews based on its "positive word counts"
@@ -47,6 +48,7 @@ hotel$Positive_Review[hotel$Review_Total_Positive_Word_Counts == 0] = ""
 # delete negative reviews based on its "negative word counts"
 hotel$Negative_Review[hotel$Review_Total_Negative_Word_Counts == 0] = ""
 
+# overall sentiment
 tidy_hotel = hotel %>%
   dplyr::select(city, Positive_Review) %>%
   unnest_tokens("word", Positive_Review) %>%
@@ -54,3 +56,34 @@ tidy_hotel = hotel %>%
   inner_join(get_sentiments("bing")) %>%
   count(city, sentiment) 
   
+# sentiment analysis across times for each city
+city_sentiments = list()
+#hotel$Review_Date = as.Date(hotel$Review_Date,format='%m/%d/%Y')
+hotel$Review_Date = mdy(hotel$Review_Date)
+# group by year_month
+hotel$Review_YearMonth = rep(0, length(hotel$Review_Date))
+hotel$Review_YearMonth = paste0(year(hotel$Review_Date), month(hotel$Review_Date))
+
+for (city in unique(hotel$city)) {
+  city_hotel = hotel[hotel$city == city, ]
+  tidy_city_hotel = city_hotel %>%
+    dplyr::select(Review_YearMonth, Positive_Review) %>%
+    unnest_tokens("word", Positive_Review) %>%
+    anti_join(stop_words) %>%
+    inner_join(get_sentiments("bing")) %>%
+    count(Review_YearMonth, sentiment) 
+  city_sentiments = append(city_sentiments, list(tidy_city_hotel))
+}
+
+positive = city_sentiments[[1]] %>%
+  filter(sentiment=="positive")
+negative = city_sentiments[[1]] %>%
+  filter(sentiment=="negative")
+ggplot()+
+  geom_line(group = 1, data = negative, aes(x = Review_YearMonth, y = n), colour = "grey")+
+  geom_line(group = 2, data = positive, aes(x = Review_YearMonth, y = n), colour="blue")+
+  theme_minimal()+
+  ylab("TODO")+
+  xlab("Date")
+
+#求每个城市中不同时间段里的评论个数 TODO！
