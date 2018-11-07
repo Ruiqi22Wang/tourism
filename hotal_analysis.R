3c7e0f466531ae031830a7768b51d3b277b54e52
chrimiway <1301235225@qq.com>
2018-11-06 19:57:27 -0500

    fix city name, sentiment analysis for hotel

--- hotal_analysis.R
+++ hotal_analysis.R
@@ -1,5 +1,7 @@
+setwd("~/Desktop/Graduate/duke/MIDS/semester 1/text-scraping/project/tourism")
 library(tidyverse)
-library(ggmap)
+library(tidytext)
+library(dplyr)
 hotel = read_csv("Hotel_Reviews.csv")
 
 ## extract address and coordinates
@@ -14,8 +16,41 @@ hotel = read_csv("Hotel_Reviews.csv")
 # write.csv(file = "coordna_city.csv", coordna_city)
 
 ## load cities
-hotel_city = read.table(file = "hotel_city.txt", sep = ",")
+hotel_city = read.table(file = "hotel_city.txt", sep = ",", stringsAsFactors = F)
 names(hotel_city) = c("Hotel_Address", "city")
 
 hotel = hotel %>%
   left_join(hotel_city)
+hotel$city = trimws(hotel$city)
+
+# fix geodecode inconsistency
+hotel$city[hotel$city == "BCN"] = "Barcelona"
+hotel$city[hotel$city == "Bobigny"] = "Paris"
+hotel$city[hotel$city == "City of London"] = "London"
+hotel$city[hotel$city == "Innere Vienna"] = "Vienna"
+hotel$city[hotel$city == "London Borough of Islington"] = "London"
+hotel$city[hotel$city == "MI"] = "Milan"
+hotel$city[hotel$city == "Milano"] = "Milan"
+
+# check "city" is contained in address
+for (i in 1:length(hotel$city)) {
+  if (!grepl(hotel$city[i], hotel$Hotel_Address[i])) {
+    print(paste(toString(i), "not matched"))
+  } else {
+    print(paste(toString(i),"matched"))
+  }
+} 
+
+
+# delete positive reviews based on its "positive word counts"
+hotel$Positive_Review[hotel$Review_Total_Positive_Word_Counts == 0] = ""
+# delete negative reviews based on its "negative word counts"
+hotel$Negative_Review[hotel$Review_Total_Negative_Word_Counts == 0] = ""
+
+tidy_hotel = hotel %>%
+  dplyr::select(city, Positive_Review) %>%
+  unnest_tokens("word", Positive_Review) %>%
+  anti_join(stop_words) %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(city, sentiment) 
+  
